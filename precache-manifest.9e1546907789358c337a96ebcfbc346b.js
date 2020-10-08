6b2b51844e7736cde619c0727066604a932dce78
Santosh Kumar <santoshkumar964887@gmail.com>
2020-10-08 11:42:16 +0530

    Updates

---
+++ precache-manifest.9e1546907789358c337a96ebcfbc346b.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "756c99c8f48620047eb61dc89c935a4c",
+    "url": "/reactjs-beer-lovers/index.html"
+  },
+  {
+    "revision": "3d7582ab5e06c5ecaa91",
+    "url": "/reactjs-beer-lovers/static/css/main.3d33bcba.chunk.css"
+  },
+  {
+    "revision": "9b419e8729d04b17c08e",
+    "url": "/reactjs-beer-lovers/static/js/2.f2a77b7b.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/reactjs-beer-lovers/static/js/2.f2a77b7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d7582ab5e06c5ecaa91",
+    "url": "/reactjs-beer-lovers/static/js/main.da54d7e6.chunk.js"
+  },
+  {
+    "revision": "79a4e49fcf04c6ff9b1f",
+    "url": "/reactjs-beer-lovers/static/js/runtime-main.904d865c.js"
+  },
+  {
+    "revision": "13685372945d816a2b474fc082fd9aaa",
+    "url": "/reactjs-beer-lovers/static/media/fa-brands-400.13685372.ttf"
+  },
+  {
+    "revision": "3debcaf1a98e4445d213fd0089d42f6b",
+    "url": "/reactjs-beer-lovers/static/media/fa-brands-400.3debcaf1.svg"
+  },
+  {
+    "revision": "a06da7f0950f9dd366fc9db9d56d618a",
+    "url": "/reactjs-beer-lovers/static/media/fa-brands-400.a06da7f0.woff2"
+  },
+  {
+    "revision": "c1868c9545d2de1cf8488f1dadd8c9d0",
+    "url": "/reactjs-beer-lovers/static/media/fa-brands-400.c1868c95.eot"
+  },
+  {
+    "revision": "ec3cfddedb8bebd2d7a3fdf511f7c1cc",
+    "url": "/reactjs-beer-lovers/static/media/fa-brands-400.ec3cfdde.woff"
+  },
+  {
+    "revision": "261d666b0147c6c5cda07265f98b8f8c",
+    "url": "/reactjs-beer-lovers/static/media/fa-regular-400.261d666b.eot"
+  },
+  {
+    "revision": "89f9806b964f92ad282fd60947eba588",
+    "url": "/reactjs-beer-lovers/static/media/fa-regular-400.89f9806b.svg"
+  },
+  {
+    "revision": "c20b5b7362d8d7bb7eddf94344ace33e",
+    "url": "/reactjs-beer-lovers/static/media/fa-regular-400.c20b5b73.woff2"
+  },
+  {
+    "revision": "db78b9359171f24936b16d84f63af378",
+    "url": "/reactjs-beer-lovers/static/media/fa-regular-400.db78b935.ttf"
+  },
+  {
+    "revision": "f89ea91ecd1ca2db7e09baa2c4b156d1",
+    "url": "/reactjs-beer-lovers/static/media/fa-regular-400.f89ea91e.woff"
+  },
+  {
+    "revision": "1ab236ed440ee51810c56bd16628aef0",
+    "url": "/reactjs-beer-lovers/static/media/fa-solid-900.1ab236ed.ttf"
+  },
+  {
+    "revision": "a0369ea57eb6d3843d6474c035111f29",
+    "url": "/reactjs-beer-lovers/static/media/fa-solid-900.a0369ea5.eot"
+  },
+  {
+    "revision": "ad912fd102f4052d3273dd838e0c671d",
+    "url": "/reactjs-beer-lovers/static/media/fa-solid-900.ad912fd1.svg"
+  },
+  {
+    "revision": "b15db15f746f29ffa02638cb455b8ec0",
+    "url": "/reactjs-beer-lovers/static/media/fa-solid-900.b15db15f.woff2"
+  },
+  {
+    "revision": "bea989e82b07e9687c26fc58a4805021",
+    "url": "/reactjs-beer-lovers/static/media/fa-solid-900.bea989e8.woff"
+  }
+]);
